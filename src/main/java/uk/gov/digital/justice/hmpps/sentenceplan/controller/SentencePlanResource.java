08740a2fc15e291960cf2f5fc62b015db1172390
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2019-08-02 15:19:15 +0100

    updating for PR - need to write tests to finish

--- src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/controller/SentencePlanResource.java
+++ src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/controller/SentencePlanResource.java
@@ -68,7 +68,7 @@ public class SentencePlanResource {
     @ApiOperation(value = "Update Sentence Plan step from ID",
             response = Step.class,
             notes = "Request a single sentence plan step")
-    ResponseEntity<Step> getSentencePlanStep(@ApiParam(value = "Sentence Plan ID") @PathVariable UUID sentencePlanUUID, @ApiParam(value = "Step ID") @PathVariable UUID stepId, @ApiParam(value = "Step details", required = true) @RequestBody @Valid UpdateSentencePlanStep step) {
+    ResponseEntity<Step> updateSentencePlanStep(@ApiParam(value = "Sentence Plan ID") @PathVariable UUID sentencePlanUUID, @ApiParam(value = "Step ID") @PathVariable UUID stepId, @ApiParam(value = "Step details", required = true) @RequestBody @Valid UpdateSentencePlanStep step) {
         sentencePlanService.updateStep(sentencePlanUUID, stepId,
                 step.getOwner(),
                 step.getOwnerOther(),
@@ -93,7 +93,7 @@ public class SentencePlanResource {
     @GetMapping(value = "/sentenceplan/{sentencePlanUUID}/steps/{stepId}", produces = "application/json")
     @ApiOperation(value = "Get Sentence Plan step from ID",
             notes = "update a single sentence plan step")
-    ResponseEntity updateSentencePlanStep(@ApiParam(value = "Sentence Plan ID") @PathVariable UUID sentencePlanUUID, @ApiParam(value = "Step ID") @PathVariable UUID stepId) {
+    ResponseEntity getSentencePlanStep(@ApiParam(value = "Sentence Plan ID") @PathVariable UUID sentencePlanUUID, @ApiParam(value = "Step ID") @PathVariable UUID stepId) {
         return ResponseEntity.ok(sentencePlanService.getSentencePlanStep(sentencePlanUUID, stepId));
     }
 
