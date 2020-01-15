c2c8f0e5b6296ba080814172a3f4d825177d291f
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2020-01-15 15:10:15 +0000

    Sw sonar issues (#78)
    * Fix issues identified by Sonar
    
    * more sonar issues fixed

--- src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/jpa/repository/MotivationRefDataRespository.java
+++ src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/jpa/repository/MotivationRefDataRespository.java
@@ -3,9 +3,7 @@ package uk.gov.digital.justice.hmpps.sentenceplan.jpa.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.digital.justice.hmpps.sentenceplan.jpa.entity.MotivationRefEntity;
-
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface MotivationRefDataRespository extends CrudRepository<MotivationRefEntity, Long> {
