a9f557c75ff4036585357009a51f2692d3235b7a
Stuart Whitlam <stuart.whitham@digital.justice.gov.uk>
2020-01-28 07:38:36 +0000

    SP-550 update motivation text and remove friendly text field which is no longer needed

--- src/test/resources/action/before-test.sql
+++ src/test/resources/action/before-test.sql
@@ -1,4 +1,3 @@
-DELETE FROM sentenceplanapitest.motivation_ref_data WHERE true;
 DELETE FROM sentenceplanapitest.need WHERE true;
 DELETE FROM sentenceplanapitest.sentence_plan where true;
 DELETE FROM sentenceplanapitest.offender WHERE true;
@@ -6,14 +5,6 @@ DELETE FROM sentenceplanapitest.offender WHERE true;
 INSERT INTO sentenceplanapitest.offender (uuid, oasys_offender_id, nomis_offender_id, delius_offender_id) VALUES ('11111111-1111-1111-1111-111111111111', 123456, null, null);
 INSERT INTO sentenceplanapitest.offender (uuid, oasys_offender_id, nomis_offender_id, delius_offender_id) VALUES ('22222222-2222-2222-2222-222222222222', 789123, null, null);
 
-INSERT INTO sentenceplanapitest.motivation_ref_data (uuid, motivation_text, friendly_text, created)
-VALUES ('38731914-701d-4b4e-abd3-1e0a6375f0b2', 'Pre-contemplation', 'In the dark', '2019-07-24'),
-('82c563d5-00de-4e17-af88-c0363c4d91cc', 'Contemplation', 'Thinking about it', '2019-07-24'),
-('55f90269-1d93-4a9a-921a-7bae73f3bf07', 'Preparation', 'Getting ready', '2019-07-24'),
-('13395347-f226-4cc3-abcf-9dbc224c0f50', 'Action', 'Doing it', '2019-07-24'),
-('42de6207-7a3a-4ffb-9b45-c3ee4ea514fa', 'Maintenance', 'Keeping going', '2019-07-24'),
-('76e7770b-2a66-40e4-88b8-ccd038501906', 'Relapse', 'Off track', '2019-07-24'),
-('2f1a5751-f6ee-450a-9b20-858500cb037e', 'Permanent exit', 'The new me', '2019-07-24');
 
 -- Plan with single Objective and Action
 INSERT INTO sentenceplanapitest.sentence_plan (uuid, created_on, created_by, started_date, completed_date, data, assessment_needs_last_imported_on, offender_uuid)
