7804e5fde53270ce8784c37fb5f98727d1d9e259
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-02-11 17:00:16 +0000

    change the POST to a PUT

--- src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/controller/SentencePlanResource.java
+++ src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/controller/SentencePlanResource.java
@@ -152,7 +152,7 @@ public class SentencePlanResource {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping(value = "/sentenceplans/{sentencePlanUUID}/objectives/{objectiveUUID}/actions/{actionUUID}", produces = "application/json")
+    @PutMapping(value = "/sentenceplans/{sentencePlanUUID}/objectives/{objectiveUUID}/actions/{actionUUID}", produces = "application/json")
     @ApiOperation(value = "Updates an Action on a Sentence Plan Objective")
     @Authorised(accessLevel = AccessLevel.WRITE_SENTENCE_PLAN)
     ResponseEntity updateAction(@ApiParam(value = "Sentence Plan ID", required = true, example = "11111111-1111-1111-1111-111111111111") @PathVariable UUID sentencePlanUUID,
