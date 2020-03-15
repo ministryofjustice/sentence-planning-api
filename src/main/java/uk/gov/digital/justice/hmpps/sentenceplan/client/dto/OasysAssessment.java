f4cf4e12cf4059dae2353b02e4816fc90730ab6d
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-03-15 23:37:02 +0000

    updating offender DTO

--- src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/client/dto/OasysAssessment.java
+++ src/main/java/uk/gov/digital/justice/hmpps/sentenceplan/client/dto/OasysAssessment.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OasysAssessment {
-    @JsonProperty("oasysSetId")
+    @JsonProperty("assessmentId")
     private long assessmentId;
 
     @JsonProperty("assessmentStatus")
